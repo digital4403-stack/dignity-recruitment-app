3f06cd6263ff89427e1d43882070bac5ac62502d
digital4403-stack <digital4403@gmail.com>
2026-05-31 00:22:04 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dignity Recruitment System</h1>
+      <p>App is running successfully 🚀</p>
+    </div>
+  );
+}
